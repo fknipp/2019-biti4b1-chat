f2e687d373dab75701f5deb4285604a6e2a56b35
Franz Knipp <franz@qnipp.com>
2019-04-13 16:59:41 +0200

    Adding MessageInput

--- imports/ui/MessageInput.js
+++ imports/ui/MessageInput.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Meteor } from 'meteor/meteor';
 
-export default MessageInput = () => (
-  <div>
-    Message Input
-  </div>
-)
+export default MessageInput = () => {
+
+  const [text, setText] = useState('');
+  const onChange = (e) => setText(e.target.value);
+  const onKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      Meteor.call('sendMessage', { text });
+      setText('');
+    }
+  };
+
+  return <div><input type="text" value={ text } onChange={ onChange } onKeyPress={ onKeyPress }/></div>
+}
